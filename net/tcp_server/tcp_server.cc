a900d9717c7f52a55fae5d2cd4551a5b4ba24c10
DreamyBlueLunar <2010614119@qq.com>
2025-01-04 02:57:27 +0000

    add tcp_server

--- net/tcp_server/tcp_server.cc
+++ net/tcp_server/tcp_server.cc
@@ -1,5 +1,11 @@
 #include "tcp_server.h"
 #include "../../base/logger/logger.h"
+#include "../tcp_connection/tcp_connection.h"
+
+#include <strings.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 static moony::event_loop* check_loop_not_null(moony::event_loop* loop) {
     if (nullptr == loop) {
@@ -25,10 +31,21 @@ moony::tcp_server::tcp_server(moony::event_loop* loop,
     // 当有新用户连接时，会执行 tcp_server::new_connection() 回调
     // 对应到代码上就是 acceptor::handle_read()
     acceptor_->set_connection_callback(
-                std::bind(&tcp_server::new_connection, 
+                std::bind(&moony::tcp_server::new_connection, 
                 this, 
                 std::placeholders::_1, 
                 std::placeholders::_2));
+}
+
+moony::tcp_server::~tcp_server() {
+    for (auto& item : connections_) {
+        // 这个局部的智能指针对象出了右括号就可以自动释放 new 出来的 tcp_connection 对象资源 
+        moony::tcp_connection_ptr conn(item.second);
+        item.second.reset();
+
+        conn->get_loop()->run_in_loop(
+            std::bind(&moony::tcp_connection::connection_destroyed, conn));
+    }
 }
 
 void moony::tcp_server::set_thread_num(int num_threads) {
@@ -39,10 +56,76 @@ void moony::tcp_server::set_thread_num(int num_threads) {
 void moony::tcp_server::start() {
     if (started_ ++ == 0) {
         thread_pool_->start(thread_init_callback_);
-        loop_->run_in_loop(std::bind(&acceptor::listen, acceptor_.get()));
+        loop_->run_in_loop(std::bind(&moony::acceptor::listen, acceptor_.get()));
     }
 }
 
-void moony::tcp_server::new_connection(int sockfd, const moony::inetaddress& addr) {
+// 有一个新的客户端连接，acceptor 会执行这个回调操作
+void moony::tcp_server::new_connection(int sockfd, 
+        const moony::inetaddress& peer_addr) {
+    // 轮询算法拿到某个 sub_loop 来管理 channel
+    moony::event_loop* io_loop = thread_pool_->get_next_loop();
+    char buf[64] = {0};
+    snprintf(buf, sizeof(buf), "-%s#%d", ip_port_.c_str(), next_id_);
+    next_id_ ++;
+    std::string conn_name = name_ + buf;
 
+    LOG_FMT_INFO(
+        "tcp_server::new_connection [%s] - new connection [%s] from %s\n",
+        name_.c_str(),
+        conn_name.c_str(),
+        peer_addr.to_ip_port().c_str());
+
+    // 通过 sockfd 获取本机 ip 和 port
+    sockaddr_in local;
+    ::bzero(&local, sizeof(local));
+    socklen_t len = sizeof(local);
+    if (::getsockname(sockfd, (sockaddr*)&local, &len) < 0) {
+        LOG_FMT_ERROR("%s:%s:%d failed to get local address\n",
+                __FILE__, __FUNCTION__, __LINE__);
+    }
+    moony::inetaddress local_addr(local);
+
+    // 根据连接成功的 sockfd 创建 tcp_connection 对象
+    moony::tcp_connection_ptr conn(
+        new moony::tcp_connection(io_loop, conn_name, 
+                            sockfd, local_addr, peer_addr));
+
+    connections_[conn_name] = conn;
+
+    // 下面的回调都是用户设置给 tcp_server => tcp_connection => channel => poller => notify channel 调用回调
+    conn->set_connection_callback(connection_callback_);
+    conn->set_message_callback(message_callback_);
+    conn->set_write_complete_callback(write_complete_callback_);
+    // 设置了如何关闭连接的回调
+    conn->set_close_callback(
+        std::bind(&moony::tcp_server::remove_connection,
+                    this, 
+                    std::placeholders::_1));
+
+    // 直接调用 tcp_connection::connection_established
+    io_loop->run_in_loop(
+        std::bind(&moony::tcp_connection::connection_established, conn));
+}
+
+void 
+moony::tcp_server::remove_connection(
+        const moony::tcp_connection_ptr& conn) {
+    loop_->run_in_loop(
+        std::bind(&moony::tcp_server::remove_connection_in_loop,
+                    this,
+                    conn));
+}
+
+void 
+moony::tcp_server::remove_connection_in_loop(
+        const moony::tcp_connection_ptr& conn) {
+    LOG_FMT_INFO(
+        "tcp_server::remove_connection_in_loop [%s] - connection %s",
+        name_.c_str(), conn->name().c_str());
+
+    connections_.erase(conn->name());
+    moony::event_loop* io_loop = conn->get_loop();
+    io_loop->queue_in_loop(
+        std::bind(&moony::tcp_connection::connection_destroyed, conn));
 }
